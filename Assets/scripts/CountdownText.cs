c5fa27ccbc8cdd77ed5b753b66fc28f2d0d130b9
Sriram <sriramashok02@gmail.com>
2021-01-11 13:01:51 -0600

    Pushing app

---
+++ Assets/scripts/CountdownText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class CountdownText : MonoBehaviour {
+
+    public delegate void CountdownFinished();
+    public static event CountdownFinished OnCountdownFinished;
+
+    Text countdown;
+
+    void OnEnable() {
+        countdown = GetComponent<Text>();
+        countdown.text = 3.ToString();
+        StartCoroutine("Countdown");
+    }
+
+    IEnumerator Countdown() {
+        yield return new WaitForSeconds(1);
+        countdown.text = 2.ToString();
+        yield return new WaitForSeconds(1);
+        countdown.text = 1.ToString();
+        yield return new WaitForSeconds(1);
+        /*
+        int count = 3;
+
+        for (int i = 0; i < count; i++) {
+            countdown.text = (count - 1).ToString();
+            //yield return new WaitForSeconds(1);
+        }
+        */
+        OnCountdownFinished();
+
+    }
+
+}
